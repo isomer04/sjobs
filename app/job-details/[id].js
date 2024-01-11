337ede27b95f7391e554b51c10e5d380036d91ad
isomer04 <43922158+isomer04@users.noreply.github.com>
2024-01-10 22:18:13 -0500

    Implemented a tabs section with sub-tabs for qualifications

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -19,6 +19,8 @@ import {
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
+const tabs = ["About", "Qulifications", "Responsibilities"];
+
 const JobDetails = () => {
   const params = useSearchParams();
   const router = useRouter();
@@ -27,8 +29,23 @@ const JobDetails = () => {
   });
 
   const [refreshing, setRefreshing] = useState(false);
+  const [active, setActiveTab] = useState(tabs[0]);
 
-  const onRefresh = () => {}
+  const onRefresh = () => {};
+
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qulifications":
+        return <Specifics
+            title="Qulifications"
+            points={data[0].job_highlights?.Qualifications ?? ['N/A']}
+        />
+      case "About":
+      case "Responsibilities":
+      default:
+        break;
+    }
+  };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -53,28 +70,33 @@ const JobDetails = () => {
       <>
         <ScrollView
           showsVerticalScrollIndicator={false}
-          refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
         >
-            {isLoading ? (
-                <ActivityIndicator size="large" color={COLORS.primary} />
-            ): error ? (
-                <Text>Something went wrong</Text>
-            ) : data.length === 0 ? (
-                <Text>No data</Text>
-            ) : (
-                <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
-                    <Company 
-                        companyLogo={data[0].employer_logo}
-                        jobTitle={data[0].job_title}
-                        companyName={data[0].employer_name}
-                        Location={data[0].job_country}
-                    />
-                    <JobTabs />
+          {isLoading ? (
+            <ActivityIndicator size="large" color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                Location={data[0].job_country}
+              />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
 
-                </View>
-            
-            )}
-
+              {displayTabContent()}
+            </View>
+          )}
         </ScrollView>
       </>
     </SafeAreaView>
